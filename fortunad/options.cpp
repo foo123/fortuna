6c67786cf20c5e11a9a5e18f43f4cd88da34ee4a
Adam Mizerski <adam@mizerski.pl>
2013-09-18 11:31:23 +0100

    cleaner options handling

--- fortunad/options.cpp
+++ fortunad/options.cpp
@@ -25,13 +25,13 @@ Application::AllConfig handle_options(int argc, char* argv[], Application::AllCo
     po::options_description options("General Options");
     options.add_options()
         ("help,h", "print this help")
-        ("socket,s", self_default_value<std::string>(&config.server.socket))
+        ("socket,s", self_default_value(&config.server.socket))
         ("threads,t", "specify number of worker threads (defaults to number of hardware threads)")
     ;
     
     po::options_description accumulator_options("Accumulator Options");
     accumulator_options.add_options()
-        ("min_pool_size,p", self_default_value<unsigned long>(&config.accumulator.min_pool_size), "minimum pool size [bytes]")
+        ("min_pool_size,p", self_default_value(&config.accumulator.min_pool_size), "minimum pool size [bytes]")
     ;
     
     options
@@ -49,7 +49,7 @@ Application::AllConfig handle_options(int argc, char* argv[], Application::AllCo
     
     if (vm.count("threads")) {
         config.application.default_threads_num = false;
-        config.application.custom_threads_num = vm["threads"].as<unsigned>();
+        config.application.custom_threads_num = vm["threads"].as<decltype(config.application.custom_threads_num)>();
     }
     
     return std::move(config);
